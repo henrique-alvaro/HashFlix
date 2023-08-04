5e570dd899948c2bc834c9409721b594f02ca58c
Henrique <henrique.coolcat15@gmail.com>
2023-08-03 21:13:38 -0300

    versao 1.0 demo

---
+++ filme/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import Homefilmes, Homepage, Detalhesfilme, Pesquisafilme, Paginaperfil, Criarconta
+from django.contrib.auth import views as auth_view
+
+app_name = 'filme'
+
+urlpatterns = [
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
+    path('pesquisa/', Pesquisafilme.as_view(), name='pesquisafilme'),
+    path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('editarperfil/', Paginaperfil.as_view(), name='editarperfil'),
+    path('criarconta/', Criarconta.as_view(), name='criarconta'),
+]
---
+++ filme/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, redirect, reverse
+from .models import Filme
+from .forms import CriarContaForm
+from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class Homepage(TemplateView):
+    template_name = 'homepage.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('filme:homefilmes')
+        else:
+            return super().get(request, *args, **kwargs)
+
+
+class Homefilmes(LoginRequiredMixin, ListView):
+    template_name = 'homefilmes.html'
+    model = Filme
+
+
+class Detalhesfilme(LoginRequiredMixin, DetailView):
+    template_name = 'detalhesfilme.html'
+    model = Filme
+
+    def get(self, request, *args, **kwargs):
+        filme = self.get_object()
+        filme.visualizacao += 1
+        filme.save()
+        usuario = request.user
+        usuario.filmes_vistos.add(filme)
+        return super().get(request, *args, **kwargs)#redireciona o usuario para url final
+
+    def get_context_data(self, **kwargs):
+        context = super(Detalhesfilme, self).get_context_data(**kwargs)
+        #filtrar a tabela de filmes cuja categoria e igual a categoria do filmes da pagina (object)
+        filmes_relacionados = self.model.objects.filter(categoria=self.get_object().categoria)[0:5]
+        context['filmes_relacionados'] = filmes_relacionados
+        return context
+
+
+class Pesquisafilme(LoginRequiredMixin, ListView):
+    template_name = 'pesquisa.html'
+    model = Filme
+
+    def get_queryset(self):
+        termo_pesquisa = self.request.GET.get('query')
+        if termo_pesquisa:
+            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
+            return object_list
+        else:
+            return None
+
+
+class Paginaperfil(LoginRequiredMixin, TemplateView):
+    template_name = 'editarperfil.html'
+
+
+class Criarconta(FormView):
+    template_name = 'criarconta.html'
+    form_class = CriarContaForm
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('filme:login')
+
+
+
+
+# url - view - html
+#def homefilmes(request):
+#    context = {}
+#    lista_filmes = Filme.objects.all()
+#    context['lista_filmes'] = lista_filmes
+#    return render(request, 'homefilmes.html', context)
+
+
+#def homepage(request):
+#    return render(request, 'homepage.html')
